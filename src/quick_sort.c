26f18f4834e803c6d5323d7c37b41bdc4fa29bc2
Endo Koya <endo.k.af@is-mac40.edu.c.titech.ac.jp>
2018-06-26 16:33:09 +0900

    abya

--- src/quick_sort.c
+++ src/quick_sort.c
@@ -6,21 +6,39 @@
 int A[N];
 
 /*
-A[0], A[1], ..., A[n-1] をソートして昇順に書き換える関数
-*/
+ A[0], A[1], ..., A[n-1] をソートして昇順に書き換える関数
+ */
 void quick_sort(int A[], int n){
+    int i, j, pivot;
+    // 先頭の要素をピボットとする
+    pivot = A[0];
+    for(i = j = 1; i < n; i++){
+        if(A[i] < pivot){
+            int z = A[j];
+            A[j] = A[i];
+            A[i] = z;
+            j++;
+        }
+    }
+    if(j>1){
+        A[0]=A[j-1];
+        A[j-1]=pivot;
+        quick_sort(A,j-1);
+    }
+    if(j<n)quick_sort(A+j,n-j);
 }
 
 int main(){
-  int i;
-  A[0] = 0;
-  A[1] = 3; //原始元
-  for(i=2;i<N;i++){
-    A[i] = (long long int) A[i-1] * A[1] % N;
-  }
-
-  quick_sort(A, N);
-  for(i=0;i<N;i++){
-    if(A[i] != i) printf("ERROR %dth element is %d\n", i, A[i]);
-  }
+    int i;
+    A[0] = 0;
+    A[1] = 3; //原始元
+    for(i=2;i<N;i++){
+        A[i] = (long long int) A[i-1] * A[1] % N;
+    }
+    
+    quick_sort(A, N);
+    for(i=0;i<N;i++){
+        if(A[i] != i) printf("ERROR %dth element is %d\n", i, A[i]);
+        //printf("%d\n",A[i]);
+    }
 }
--- src/quick_sort_improved.c
+++ src/quick_sort_improved.c
@@ -6,23 +6,42 @@
 int A[N];
 
 /*
-A[0], A[1], ..., A[n-1] をソートして昇順に書き換える関数
-*/
+ A[0], A[1], ..., A[n-1] をソートして昇順に書き換える関数
+ */
 void quick_sort(int A[], int n){
+    int i, j, pivot;
+    int numpivot=1;
+    // 先頭の要素をピボットとする
+    pivot = A[0];
+    for(i = j = 1; i < n; i++){
+        if(A[i] < pivot){
+            int z = A[j];
+            A[j] = A[i];
+            A[i] = z;
+            j++;
+        }
+    }
+    A[0]=A[j-1];
+    A[j-1]=pivot;
+    for(i=j;i<n;i++)if(A[i]==pivot){A[i]=A[j-1+numpivot];A[j-1+numpivot]=pivot;numpivot++;}
+    if(j>1){
+        quick_sort(A,j-1);
+    }
+    if(j+numpivot-1<n)quick_sort(A+j+numpivot-1,n-j-numpivot+1);
 }
 
 int main(){
-  int i;
-  A[0] = 0;
-  A[1] = 3; //原始元
-  for(i=2;i<N;i++){
-    A[i] = (long long int) A[i-1] * A[1] % N;
-  }
-
-// すべての要素が同じ場合でも計算が早く終わるか確認する
-
-  quick_sort(A, N);
-  for(i=0;i<N;i++){
-    if(A[i] != i) printf("ERROR %dth element is %d\n", i, A[i]);
-  }
+    int i;
+    A[0] = 0;
+    A[1] = 3; //原始元
+    for(i=2;i<N;i++){
+        //A[i] = (long long int) A[i-1] * A[1] % N;
+        A[i]=3;
+    }
+    
+    quick_sort(A, N);
+    for(i=0;i<N;i++){
+        if(A[i] != i) printf("ERROR %dth element is %d\n", i, A[i]);
+        //printf("%d\n",A[i]);
+    }
 }
